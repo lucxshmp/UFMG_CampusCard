74b792b4bc8e298f7b7771421afc9b4df4d490f1
anna <135443614+annanrod@users.noreply.github.com>
2023-12-02 01:23:36 -0300

    atualização da chamada do método de redirecionamento

--- SRC/classes_mapas/MapasCampus.cpp
+++ SRC/classes_mapas/MapasCampus.cpp
@@ -5,10 +5,6 @@
 
 namespace media::ui{
 
-    Mapa mapaPampulha("https://drive.google.com/file/d/1ML72QfOedus6YnM-UgALSKkp7HFLrb63/view?usp=sharing");
-    Mapa mapaCentroBH("https://drive.google.com/file/d/1bF_fW7p3NQlxUp4DcKES9LxjcazGCEq6/view?usp=sharing");
-    Mapa mapaMontesClaros("https://drive.google.com/file/d/1rO9XVIuv9Mv3pPTTjHwpiNAAejZNeUOH/view?usp=sharing");
-
 /// @brief construtor com todas as opcoes do menu principal 
     MapasCampus::MapasCampus(){
         _title = "Mapas da UFMG - Campi";
@@ -20,18 +16,15 @@ namespace media::ui{
     Menu *MapasCampus::next(unsigned option) {
         switch (option) {
             case 1:
-                mapaPampulha.exibirMapa();
-                std::cout << "Abrindo no navegador...\n";
+                Redirecionamento::redirecionarLink("https://drive.google.com/file/d/1ML72QfOedus6YnM-UgALSKkp7HFLrb63/view?usp=sharing");
                 return nullptr; 
 
             case 2:
-                mapaCentroBH.exibirMapa();    
-                std::cout << "Abrindo no navegador...\n";
+                Redirecionamento::redirecionarLink("https://drive.google.com/file/d/1bF_fW7p3NQlxUp4DcKES9LxjcazGCEq6/view?usp=sharing");
                 return nullptr;
                  
             case 3:
-                mapaMontesClaros.exibirMapa();    
-                std::cout << "Abrindo no navegador...\n";
+                Redirecionamento::redirecionarLink("https://drive.google.com/file/d/1rO9XVIuv9Mv3pPTTjHwpiNAAejZNeUOH/view?usp=sharing");
                 return nullptr;
 
             default:
