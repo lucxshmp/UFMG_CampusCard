f1dabe01978e67740b2c1028a0f5c1f5fb5ea672
anna <135443614+annanrod@users.noreply.github.com>
2023-12-01 23:05:17 -0300

    reupdate para correção de nome

---
+++ Include/classes_mapas/MapasInterno.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "menuMapas.hpp"
+#include "mapa.hpp"
+#include <string>
+#include <iostream>
+
+namespace media::ui {
+    class MapasInterno : public Menu {
+    public:    
+        ///@brief: constroi um menu de opções
+        MapasInterno();
+
+        ///@brief abre as opções dos usuários
+        ///@return o mapa escolhido pelo usuario
+        Menu *next(unsigned option) override;
+    };
+
+
+}
