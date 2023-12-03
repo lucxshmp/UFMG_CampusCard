420ab53f5fcaabb88993c18b4e190274c1979f94
Rebeca Cândida dos Santos <anachris154@gmail.com>
2023-12-03 17:53:40 -0300

    commit teste

--- Include/classes_transporte/onibus.hpp
+++ Include/classes_transporte/onibus.hpp
@@ -1,5 +1,3 @@
-// onibus.hpp
-
 #pragma once
 
 #include <string>
@@ -8,9 +6,17 @@
 #include <vector>
 #include <ctime>
 
+// Função para validar o formato da hora
+bool validarFormatoHora(const std::string& horaStr);
+
 struct HorarioOnibus {
-    std::string placa;
     std::chrono::system_clock::time_point horario;
+    std::tm tmObj;
+    std::string placa;  // Adicionando o membro 'placa'
+    std::string tipo;
+
+    // Construtor padrão
+    HorarioOnibus() : horario(std::chrono::system_clock::now()), tipo("") {}
 };
 
 class Onibus {
@@ -19,12 +25,22 @@ private:
     std::string placa;
     std::string marca;
     std::string tipo;
+    std::string placaAtual;
+
+    std::vector<HorarioOnibus> horarios;  // Modificado para armazenar HorarioOnibus
+
+    void salvarOnibus() const;
+    void cadastrarOnibusNovo();
+    std::string formatarOnibus(const HorarioOnibus& horario) const;
 
 public:
-    std::vector<HorarioOnibus> horarios;
-
     void cadastrarOnibus();
     void exibirInformacao() const;
-    void exibirHorarios();
-    const std::vector<HorarioOnibus>& getHorarios() const;
+    void exibirHorarios() const;
+    void cadastrarHorarios(HorarioOnibus& novoHorario);
+    void editarOnibus();
+    void exibirOnibusCadastrados() const;
+    void carregarOnibus();
+
+    const std::vector<HorarioOnibus>& getOnibusCadastrados() const;
 };
