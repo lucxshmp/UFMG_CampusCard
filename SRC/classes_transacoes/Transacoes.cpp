4cabbaf9bdceb1442353412fe8e95abdf27d0011
Erik Rocha <erikrocha98@hotmail.com>
2023-12-03 16:56:27 -0300

    implementação do método deposito no arquivo transacoes.cpp

--- SRC/classes_transacoes/Transacoes.cpp
+++ SRC/classes_transacoes/Transacoes.cpp
@@ -6,13 +6,23 @@ Usuario usuario;
 
 Transacoes::Transacoes(){
     confirma_pagamento=false;
+    confirma_deposito=false;
 }
 void Transacoes::pagamento(){
     if (confirma_pagamento==true){
         saldo.diminuir_saldo(usuario.getNivelFump());
-        std::cout<<"pagamento confirmado"<<std::endl;
+        std::cout<<"Pagamento confirmado"<<std::endl;
     }
     else{
         std::cout<<"Houve um erro no pagamento"<<std::endl;
+    }
+}
+void Transacoes::deposito(){
+    if (confirma_deposito==true){
+        saldo.adicionar_saldo(0); //O zero é apenas um valor qualquer para completar a lógica da transação. Será modificado.
+        std::cout<<"Deposito confirmado"<<std::endl;
+    }
+    else{
+        std::cout<<"Houve um erro no deposito"<<std::endl;
     }
 }
