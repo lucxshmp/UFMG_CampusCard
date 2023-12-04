0f84819c8f249c09c77068e699d787206b811273
Rebeca Cândida dos Santos <anachris154@gmail.com>
2023-12-04 18:18:36 -0300

    alterações finais na classe onibus

--- SRC/classes_menu/menuOnibus.cpp
+++ SRC/classes_menu/menuOnibus.cpp
@@ -2,51 +2,47 @@
 #include "onibus.hpp"
 #include "menuOnibus.hpp"
 #include "menuVans.hpp"
-#include "interface.hpp"
 
-namespace media::ui{
+namespace media{
+  namespace ui {
 /// @brief construtor com todas as opcoes do menu principal 
-    MenuOnibus::MenuOnibusVans(){
-        _title = "\n\n********** MENU ONIBUS **********\n\n ";
-        _title = "Escolha uma opção: \n";
+    MenuOnibus::MenuOnibus() {
+        _title = "MENU ONIBUS";
+        std::cout<<  "Escolha uma opção: \n";  
         _options.push_back("1. Cadastrar ônibus");
         _options.push_back("2. Exibir ônibus cadastrados");
         _options.push_back("3. Editar ônibus");
-        _options.push_back("0. Sair");
         std::cout << "> ";
     }
 
     Menu *MenuOnibus::next(unsigned option) {
-        do{
+      Onibus meuOnibus;
         switch (option) {
-            
-            case 0:
-                Interface::mensagemSaida();
-                return nullptr;
-
             case 1:
-                
-                meuOnibus.cadastrarOnibus();
-                meuOnibus.exibirInformacao();
-                return nullptr;
+                  meuOnibus.cadastrarOnibus();
+                  meuOnibus.exibirInformacao();
+              return new MenuOnibus();
 
             case 2:
 
-                meuOnibus.exibirOnibusCadastrados();
-                return MenuOnibusVans();
+              meuOnibus.exibirOnibusCadastrados();
+              return new MenuOnibus();
 
-            case 3: 
-                
-                meuOnibus.editarOnibus();
-                return nullptr; 
+          case 3:
+          std::cin.ignore();  // Limpa o buffer de entrada
+          meuOnibus.editarOnibus();
+          return new MenuOnibus(); 
+
+            case 0:
+            std::cout << "Saindo..." << std::endl;
+              return nullptr;
+            break;
 
             default:
-                // Opção inválida, retorna nullptr para passar na compilaçao
-                std::cout << "Opcao invalida! Digite novamente:\n" << std::endl;
-                std::cin >> option;  // leia novamente a entrada
-                return nullptr;
+                std::cout << "Opcao invalida!" << std::endl;
+              return nullptr;
         }
-        } while (option < 0 || option > 3);
     }
+  }
 };
 
--- SRC/classes_menu/menuVans.cpp
+++ SRC/classes_menu/menuVans.cpp
@@ -1,47 +1,47 @@
 #include "menuTransporte.hpp"
 #include "van.hpp"  // Altere o nome do arquivo para van.hpp
 #include "menuVans.hpp"  // Altere o nome do arquivo para menuVan.hpp
-#include "interface.hpp"
 
-namespace media::ui {
+namespace media{ 
+  namespace ui {
     /// @brief construtor com todas as opcoes do menu principal 
-        MenuVans::MenuVans(){
-        _title = "\n\n********** MENU VANS **********\n\n ";
-        _title = "Escolha uma opção: \n";
+        MenuVans::MenuVans(){ 
+        _title = " MENU VANS";
+          std::cout << "\nEscolha uma opção: \n";
         _options.push_back("1. Cadastrar van");
         _options.push_back("2. Exibir vans cadastradas");
         _options.push_back("3. Editar van");
-        _options.push_back("0. Sair");
         std::cout << "> ";
-    }
+        }
 
-        Menu *MenuVans::next(unsigned option) {
-        do {
-        switch (option) {
-
-            case 0:
-                Interface::mensagemSaida();
-                return nullptr;
-
+        Menu *MenuVans::next(unsigned option){;
+          Van minhaVan;
+          switch (option) {
             case 1:
+                std::cin.ignore();
                 minhaVan.cadastrarVan();
                 minhaVan.exibirInformacao();
-                return nullptr;
+                return new MenuVans();
 
             case 2:
                 minhaVan.exibirVansCadastradas();
-                return MenuOnibusVans();
-
+                return new MenuVans();
+            
             case 3:
+                std::cin.ignore();
                 minhaVan.editarVan();
                 return nullptr;
 
-           default:
-                 // Opção inválida, retorna nullptr para passar na compilaçao
-                std::cout << "Opcao invalida! Digite novamente:\n" << std::endl;
-                std::cin >> option;  // leia novamente a entrada
+            case 0:
+                std::cout << "Saindo..." << std::endl;
+              return nullptr;
+                break;
+
+            default:
+                // Opção inválida, retorne nullptr ou um menu padrão
+                std::cout << "Opcao invalida!" << std::endl;
                 return nullptr;
         }
-        } while ( option < 0 || option > 3);
+        }
     }
-};
+  }
